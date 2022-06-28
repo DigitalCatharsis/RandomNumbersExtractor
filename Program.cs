da866e708ce8abf84dd8d0a8c91770a3419698e7
DigitalCatharsis <shinegumy@gmail.com>
2022-06-28 20:39:05 +0300

    Soul
    implemented input output
    (next step is threds)

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RandomNumbersExtractor
 {
@@ -13,28 +14,62 @@ namespace RandomNumbersExtractor
 
         static void Main()
         {
-            var _reader = new Generator();
+            var _reader = new DivineObject();
             _reader.ReadInput();
-            _reader.GenerateDigits();
+            _reader.Derive();
+
+
+
+            //using (var sr = new StreamReader(@"C:\TEMP\test.txt"))
+            //{
+
+            //    while ((! sr.EndOfStream ))
+            //    {
+            //        Console.WriteLine(sr.ReadLine());
+            //    }
+            //}
+
 
             Console.ReadLine();
         }
     }
 }
 
-public class Generator
+public class DivineObject
 {
     private string DigitInput { get; set; }     //а зачем я сделал свойство, а не Private переменную? Мозги кипят...
     private string SaveMethInput { get; set; }     //а зачем я сделал свойство, а не Private переменную? Мозги кипят...
-    private int  DigitResult { get; set; }
-    private int  SaveMethResult { get; set; }
-    public void ReadInput()
+    private int DigitResult { get; set; }
+    private string PathInputResult { get; set; }
+
+    public void ReadInput()   //Метод для получения данных с консоли
     {
         CheckDigitValidation();
-        SaveMethodValidation();
+        CheckMethodValidation();
+        if (SaveMethInput == "y")
+        {
+            CheckPathValidation();
+        }
     }
 
-    private void CheckDigitValidation()
+    public void Derive() //Метод для вывода в файл или на консоль в зависимости от SaveMethInput
+    {
+        if (DigitInput != null || SaveMethInput != null)
+        {
+            if (SaveMethInput == "y")
+            {
+                SaveToFile(PathInputResult);
+            }
+            else
+            {
+                PrintDigits();
+            }
+        }
+        else throw new Exception("Null input");
+    }
+
+    
+    private void CheckDigitValidation() //Проверка, что вбиваешь именно цифры
     {
         var result = 0;
         var validInput = false;
@@ -54,11 +89,10 @@ public class Generator
             }
         }
         DigitResult = result;
-    }    
-    
-    private void SaveMethodValidation()
+    }
+
+    private void CheckMethodValidation() //Проверка что только Char y или n
     {
-        var result = 0;
         var validInput = false;
         while (!validInput)
         {
@@ -78,60 +112,72 @@ public class Generator
                     validInput = true;
                 }
             }
-
         }
-        SaveMethResult = result;
     }
-        
-
- public void GenerateDigits()
+    
+    private void CheckPathValidation()   //Можно реализовать попытку создания папки, оставлю метод ниже
     {
+        var validInput = false;
+        while (!validInput)
+        {
+            //get input 
+            Console.WriteLine(@"\n\t\t\t Type the file location. Example: C:\temp  ");
+            PathInputResult = Console.ReadLine();
+            //validate it
+            if (!Directory.Exists(PathInputResult))
+            {
+                Console.WriteLine(@"\n\t\t\t Try again, dummy! Example: C:\temp  ");
+            }
+            else
+            {
+                validInput = true;
+            }
+        }
+    }
+
+    private void SaveToFile(string path)    //Save to file, obvious
+    {
+        using (var sw = new StreamWriter(path + @"\out.txt", false))
+        {
+            // Instantiate random number generator using system-supplied value as seed.        
+            Random rand = new Random();
+            int _randMin = 0;
+            int _randMax = 100;
+
+            // Generate and display N random integers from _randMin to _randMax.
+            Console.WriteLine(DigitInput + " random integers between " + _randMin + " and " + _randMax);
+
+            for (int ctr = 0; ctr < DigitResult; ctr++)
+            {
+                sw.WriteLine("{0,8:N0}", rand.Next(_randMin, _randMax));
+            }
+        }
+    }
+
+    private void PrintDigits()  //Вывод реализация
+    {
+        // Instantiate random number generator using system-supplied value as seed.        
+        Random rand = new Random();
         int _randMin = 0;
         int _randMax = 100;
 
-        // Instantiate random number generator using system-supplied value as seed.
-        Random rand = new Random();
+        // Generate and display N random integers from _randMin to _randMax.
+        Console.WriteLine(DigitInput + " random integers between " + _randMin + " and " + _randMax);
 
-
-        if (SaveMethResult == 'n')
-        {
-            // Generate and display N random integers from _randMin to _randMax.
-            Console.WriteLine(DigitInput + " random integers between " + _randMin + " and " + _randMax);
             for (int ctr = 0; ctr < DigitResult; ctr++)
             {
                 Console.Write("{0,8:N0}", rand.Next(_randMin, _randMax));
             }
-        }
-        //else
-        //{
-
-        //}
-
     }
 
- //public void TypeToFile()
- //   {
- //       public static async Task ExampleAsync()
- //   {
- //       string[] lines =
- //       {
- //           "First line", "Second line", "Third line"
- //       };
+    static void CheckAndCreateDiR(string destination)  //not sure i have to implement dis
+    {
+        if (!Directory.Exists(destination))
+        {
+            Directory.CreateDirectory(destination);
+        }
 
- //       await File.WriteAllLinesAsync("WriteLines.txt", lines);
- //   }
-//}
-
-
-
-//    public void ExtractResult()
-//    {
-
-
-
-//    }
-
-
+    }
 }
 
 
