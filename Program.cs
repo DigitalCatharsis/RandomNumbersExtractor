126adc01ff419419158697416c8cc21c29bd1ba5
DigitalCatharsis <shinegumy@gmail.com>
2022-06-29 13:47:50 +0300

    Soul
    Added threads

--- Program.cs
+++ Program.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
+
+#region Таск
+//Написать программу, которая выведет указанное число рандомных значений в файл или консоль.
+//Требования:
+//1.Число рандомных чисел, которое надо вывести, считывается из консоли +
+//2. Куда выводим, тоже указывается в консоли, если это файл, то выводим в out.txt +
+//3. Проверка на валидность данных. Если данные невалидны, запрашиваем повторно +
+//4. Программа должна уметь выводь вплоть до 1000000000 значений +
+//5. Вывод должен происходить в отдельном потоке, чтобы не блокировать основной поток. Т.е. Write должен вернуть управление сразу же +
+//6. Пока идет запись, программа должна раз в секунду вызывать UpdateStatus, чтобы пользователь видел, что процесс идет
+//7. UpdateStatus должен:
+//7.1.Для консоли - обновлять заголовок консоли, выводя туда время прошедшее с начала записи
+//7.2. Для файла - выводить точку в консоль
+//7. Классы для вывода должны наследоваться от интерфейса:
+//public interface INumberWriter
+//{
+//    event Action OnComplete;
+
+//    void Write(IEnumerable<int> numbers);
+//    void UpdateStatus();
+//}
+//8.Для старта записи, в классе Program должен быть реализован метод StartWriting, принимающий два параметра: класс, который будет выводить данные и сам набор данных
+//9. После того, как запись будет завершена, вывести в консоль "Done" и время, которое запись заняла
+
+//Tips:
+//1.Для запуска функции в отдельном потоке используется класс Thread. Пример использования, в твоем случее:
+//void ThreadFunc()
+//{
+
+//}
+//new Thread(ThreadFunc).Start();
+
+//2.Чтобы корректно остановить программу, в классе Program объяви переменную:
+//   private static volatile bool s_shouldStop;
+//Проверяй ее в цикле который вызывает UpdateStatus
+//   Выстави ее в обработчике завершения процесса записи
+
+//3. Заголовок консоли меняется через Console.Title
+
+//хотя, забей на 8 пункт, он тут не нужен
+#endregion region
 
 namespace RandomNumbersExtractor
 {
     internal class Program
     {
-
-
-
         static void Main()
         {
             var _reader = new DivineObject();
@@ -56,13 +95,16 @@ public class DivineObject
     {
         if (DigitInput != null || SaveMethInput != null)
         {
+            
             if (SaveMethInput == "y")
             {
-                SaveToFile(PathInputResult);
+                Thread threadSaveToFile = new Thread(new ParameterizedThreadStart(SaveToFile));
+                threadSaveToFile.Start(PathInputResult);
             }
             else
             {
-                PrintDigits();
+                Thread threadPrintDigits = new Thread(new ThreadStart(PrintDigits));
+                threadPrintDigits.Start();
             }
         }
         else throw new Exception("Null input");
@@ -145,7 +187,7 @@ public class DivineObject
         }
     }
 
-    private void SaveToFile(string path)    //Save to file, obvious
+    private void SaveToFile(object path)    //Save to file, obvious
     {
         using (var sw = new StreamWriter(path + @"\out.txt", false))
         {
@@ -159,7 +201,7 @@ public class DivineObject
 
             for (int ctr = 0; ctr < DigitResult; ctr++)
             {
-                sw.WriteLine("{0,8:N0}", rand.Next(_randMin, _randMax));
+                sw.Write("{0,8:N0}", rand.Next(_randMin, _randMax));
             }
         }
     }
@@ -180,14 +222,14 @@ public class DivineObject
             }
     }
 
-    static void CheckAndCreateDiR(string destination)  //not sure i have to implement dis
-    {
-        if (!Directory.Exists(destination))
-        {
-            Directory.CreateDirectory(destination);
-        }
+    //static void CheckAndCreateDiR(string destination)  //not sure i have to implement dis
+    //{
+    //    if (!Directory.Exists(destination))
+    //    {
+    //        Directory.CreateDirectory(destination);
+    //    }
 
-    }
+    //}
 }
 
 
